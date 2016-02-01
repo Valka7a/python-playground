f08d4374f2cec2e8b1afec3753854b1ec10ff480
Valentin Durvenqkov <Valka7a@local103.bcserdika.com>
2016-02-01 09:58:13 +0200

    Some Fixes
    Signed-off-by: Valentin Durvenqkov <Valka7a@local103.bcserdika.com>

--- projects/crawler/doc.py
+++
@@ -1,6 +0,0 @@
-# 1 -> trqbva da namerish nachin da svalish source-a na stranicata v edna promenliva
-# 2 -> trqbva da namerish biblioteka koqto sluji za obrabotka na HTML/DOM s koqto da parse-nesh dannite koito sa vytre v source-a
-# 3 -> izvajdash s neq otdelnite elementi, ime, marka, cena, description, urli-ite na snimkite
-# 4 -> za vseki edin url ot snimka namirash kak da go svalish v dadena direktoriq
-# 5 -> zapazvash cqlata informaciq v edin json file
-# 6 -> i otdelno q pokazvash na ekrana
--- projects/rss-feed/ex2.py
+++
@@ -1,6 +0,0 @@
-import feedparser
-   2
-   3 python_wiki_rss_url = "http://www.python.org/cgi-bin/moinmoin/" \
-   4                       "RecentChanges?action=rss_rc"
-   5
-   6 feed = feedparser.parse( python_wiki_rss_url )
